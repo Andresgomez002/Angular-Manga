cfa48efb36f11e38d19c3535cf0e52957fe865fc
Andres Gomez <nicolasgomez8873030@gmail.com>
2023-07-12 20:57:09 -0500

    [+] saludo.components: crea un componente de forma manual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { saludo } from './components/saludo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    saludo
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/saludo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'como-quiero',
+    templateUrl: './saludo.components.html',
+    styleUrls:['./saludo.component.css']
+})
+export class saludo {
+    saludo = 'Hello World since angular ';
+}
