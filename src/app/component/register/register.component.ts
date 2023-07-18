47ce5ffcd0309ec13bbd097ca6359ea953343d65
Andres Gomez <nicolasgomez8873030@gmail.com>
2023-07-17 20:38:14 -0500

    [+] app.module: importamos reactiveFormsmodule [+] register.component: implementamos formularios reactivos validacion

--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-
+  registerForm: FormGroup = this.fb.group({
+    nombre: [
+      '',
+      [
+        Validators.required
+      ]
+    ],
+    email:[
+      '',
+      [
+        Validators.required,
+        Validators.email
+      ]
+    ],
+    password:[
+      '',
+      [
+        Validators.required,
+        Validators.minLength(8)
+      ]
+    ]
+  })
+constructor(private fb: FormBuilder){}
+register(){
+console.group('registerform');
+console.info(this.registerForm.value);
+console.info(this.registerForm.valid);
+console.groupEnd();
+}
 }
