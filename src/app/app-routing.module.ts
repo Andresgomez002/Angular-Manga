d0187d8a04238ea05df9c295c1aa17ec8a4c9518
Andres Gomez <nicolasgomez8873030@gmail.com>
2023-07-17 21:20:43 -0500

    [+] ng g m protected --routing [+] n g c protected/pages/dashboard [+] app.routing.module: definir ruta de modulo 'dashboard [+]protected.routing.module: definir rutas hijas del modulo 'dashboard''

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,11 @@ const routes: Routes = [
   {path: '', component: HomeComponent }, //home http://localhost:4200/
   {path: 'login', component: LoginComponent },//login http://localhost:4200/login
   {path: 'register', component: RegisterComponent},//register http://localhost:4200/register
+  {
+    path: 'dashboard',
+    loadChildren: ()=> import ("./protected/protected.module")
+    .then(module => module.ProtectedModule)
+  },
   {path: '**', redirectTo: ''} //hola -> home http://localhost:4200/
 
 ];
